13d418d000bc75cca89f8ca45116c57586be5aac
Gordon Smith <gordonjsmith@gmail.com>
2017-05-18 06:53:00 +0100

    GH-5 Add WsServices
    * wsWorkunits
    * wsDFU
    * wsSMC
    * wsTopology
    
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- src/index-common.ts
+++ src/index-common.ts
@@ -1,3 +1,11 @@
 export const version = "0.0.1";
 
+//  Util/Collections  - Should be its own package?
+export { logger, scopedLogger, Level } from "./util/logging";
 export { Connection, RequestType, ResponseType } from "./comms/connection";
+
+//  Raw Services  ---
+export { Service as WsWorkunits } from "./esp/services/wsWorkunits";
+export { Service as WsTopology } from "./esp/services/wsTopology";
+export { Service as WsSMC } from "./esp/services/wsSMC";
+export { Service as WsDFU } from "./esp/services/wsDFU";
--- test/index-common.ts
+++ test/index-common.ts
@@ -1,10 +1,16 @@
 //  Collections  ---
+import "./collections/dictionary";
 import "./collections/stack";
 
 //  Util  ---
 import "./util/logging";
+import "./util/object";
 import "./util/string";
 import "./util/url";
 
 //  Comms  ---
 import "./comms/connection";
+import "./esp/comms/connection";
+import "./esp/services/wsDFU";
+import "./esp/services/wsTopology";
+import "./esp/services/wsWorkunits";
